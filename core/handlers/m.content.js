78b9e2bbf1ae5b376cced1f4d1b385817776311d
Zeppth <151110530+Zeppth@users.noreply.github.com>
2025-12-05 18:09:12 -0500

    Add files via upload

--- core/handlers/m.content.js
+++ core/handlers/m.content.js
@@ -28,7 +28,10 @@ export default async ({ m, sock, message }) => {
     m.content = {
         text: m_content_text,
         args: m_content_text?.trim().split(/ +/),
-        media: (m.type == 'imageMessage' || m.type == 'videoMessage') ? {
+        media: (
+            m.type == 'imageMessage'
+            || m.type == 'videoMessage'
+        ) ? {
             mimeType: message?.message?.[m.type]?.mimetype || '',
             fileName: message?.message?.[m.type]?.filename || '',
             download: async () => await sock.downloadMedia(message)
@@ -36,39 +39,65 @@ export default async ({ m, sock, message }) => {
     }
 
     // QUOTED
-    let quotedMessage = false
+
     if (contextInfo.quotedMessage) {
-        quotedMessage = {
-            key: {
+        if (global.config.saveHistory) {
+            m.quoted.message = await sock.loadMessage(
+                m.chat.id, contextInfo.stanzaId)
+        } else {
+            m.quoted.message = {}
+            m.quoted.message.key = {
                 remoteJid: contextInfo.remoteJid || message.key.remoteJid,
                 fromMe: contextInfo.participant == sock.user.id.split(":")[0] + "@s.whatsapp.net",
                 participant: contextInfo.participant,
                 id: contextInfo.stanzaId
-            },
-            message: {
+            }
+            m.quoted.message.message = {
                 ...contextInfo.quotedMessage
             }
         }
 
-        m.quoted = { id: quotedMessage.key.id }
+
+        const quoted = m.quoted.message
+        m.quoted = { id: quoted.key.id }
+
         m.quoted.type = (() => {
-            for (const type of Object.keys(messageExtractors)) {
-                if (quotedMessage.message[type]) return type;
+            for (const type of Object.keys({
+                'imageMessage': 'image',
+                'videoMessage': 'video',
+                'stickerMessage': 'sticker',
+                'documentMessage': 'document',
+                'audioMessage': 'audio',
+                ...messageExtractors
+            })) {
+                if (quoted.message[type])
+                    return type;
             }
         })()
 
+        m.quoted.type ||= Object.keys(quoted.message)[0]
+        
         const quoted_content_text = await (async () => {
-            try { return messageExtractors[m.type](quotedMessage.message) || '' }
-            catch (e) { return '' }
+            try {
+                const fun = messageExtractors[m.quoted.type]
+                if (fun) return fun(quoted?.message) || ''
+                else return ''
+            } catch (e) { return '' }
         })()
 
         m.quoted.content = {
-            text: quoted_content_text,
+            text: quoted_content_text || '',
             args: quoted_content_text?.trim().split(/ +/),
-            media: (m.quoted.type == 'imageMessage' || m.quoted.type == 'videoMessage') ? {
-                mimeType: quotedMessage?.message?.[m.type]?.mimetype || '',
-                fileName: quotedMessage?.message?.[m.type]?.filename || '',
-                download: async () => await sock.downloadMedia(quotedMessage)
+            media: (
+                quoted.message.imageMessage
+                || quoted.message.videoMessage
+                || quoted.message.stickerMessage
+                || quoted.message.documentMessage
+                || quoted.message.audioMessage
+            ) ? {
+                mimeType: quoted.message?.[m.quoted.type]?.mimetype || '',
+                fileName: quoted.message?.[m.quoted.type]?.filename || '',
+                download: async () => await sock.downloadMedia(m.quoted.message)
             } : false
         }
     }
@@ -76,7 +105,6 @@ export default async ({ m, sock, message }) => {
 
     return {
         contextInfo,
-        messageExtractors,
-        quotedMessage
+        messageExtractors
     }
 }
