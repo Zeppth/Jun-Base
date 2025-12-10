2f4577448ba3fbd86e78835144c9e5392a0f4bc9
Zeppth <151110530+Zeppth@users.noreply.github.com>
2025-12-10 13:33:03 -0500

    Add info command to test.plugin.js

--- plugins/test.plugin.js
+++ plugins/test.plugin.js
@@ -1,1 +1,12 @@
-
+export default {
+    case: 'info',
+    command: true,
+    async script(m) {
+        await m.reply([
+            `*${global.config.name}*`,
+            `Versión: ${global.$package.version}`,
+            `Prefijos: ${global.config.prefixes}`,
+            `Historial: ${global.config.saveHistory ? 'Sí' : 'No'}`
+        ].join('\n'));
+    }
+}
