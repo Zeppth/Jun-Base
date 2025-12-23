6d5a1e3e8f9420af2383eeb8b691f0d2bac38f3a
Zeppth <151110530+Zeppth@users.noreply.github.com>
2025-12-22 20:02:37 -0500

    Add files via upload

--- core/config.js
+++ core/config.js
@@ -2,7 +2,7 @@
 
 import path from 'path';
 import fs from 'fs/promises';
-import base from '../library/db.js';
+import { JunDB } from '../library/db.js';
 
 const { proto } = (await import('@whiskeysockets/baileys/WAProto/index.js')).default;
 
@@ -10,12 +10,19 @@ global.$package = await fs.readFile(path.resolve('package.json'))
     .then(data => JSON.parse(data))
 
 global.$dir_main = {
+    plugins: path.resolve('./plugins'),
     creds: path.resolve('./storage/creds'),
     store: path.resolve('./storage/store'),
     temp: path.resolve('./storage/temp'),
 }
 
-global.db = base;
+global.db = new JunDB();
 global.$proto = proto;
-
 global.$dir_bot = {}
+
+await global.db.init({
+    folder: global
+        .$dir_main.store,
+    memoryLimit: 15,
+    memoryTTL: 30
+})
--- core/main.js
+++ core/main.js
@@ -1,143 +1,95 @@
 // ./core/main.js
 
-import {
-    DisconnectReason,
-    // makeInMemoryStore
-} from '@whiskeysockets/baileys';
-import qrTerminal from 'qrcode-terminal';
-import { Boom } from '@hapi/boom';
-import qrCode from 'qrcode';
+import fs from 'fs';
+import path from 'path';
+import chalk from 'chalk';
+chalk.level = 2
 
-// from libreria
-import logger from '../library/log.js';
-import $base from '../library/db.js';
-import { MakeBot } from '../library/client.js';
-import { Plugins } from '../library/plugins.js';
-import $process from '../library/process.js';
-import $Sock from '../library/bind.js';
+await import('../config.js')
+await import('./config.js')
 
-import handlerLoader from '../library/loader.js';
+/*INDEX*/ {
+    const folder = path.resolve(`./storage/`)
 
-const objects = {}
-
-try {
-    // tmp
-    await import('../library/purge.js')
-
-    const { data, path: _path } = $process.env
-    await $base.start($process.env.path.store)
-
-    await handlerLoader.loadFiles();
-
-    const plugins = new Plugins(_path.plugins, {
-        usePrefix: true,
-        stubtype: false,
-        command: false,
-    });
-
-    await plugins.load()
-
-    // start
-    async function StartBot(params) {
-        const sock = await MakeBot(params ??
-            $process.env.options, objects.store)
-
-        Object.assign(sock, {
-            plugins: plugins,
-            ...(await $Sock(sock)),
-            '@send': $process.send,
-            subBot: data.subBot || false,
-        })
-
-        if (sock.PairingCode) $process.send({
-            content: {
-                event: 'pairing:pin-code',
-                data: {
-                    pairingCode: sock.PairingCode,
-                    formattedCode: sock.PairingCode
-                        .match(/.{1,4}/g)?.join("-") ??
-                        sock.PairingCode
-                }
-            }
-        })
-
-        sock.ev.on('connection.update', async (update) => {
-            const { lastDisconnect, connection, qr } = update;
-
-            if (connection === 'close') {
-                const reason = new Boom(lastDisconnect?.error)?.output?.statusCode || 500;
-                if (reason === DisconnectReason.restartRequired) {
-                    await StartBot({
-                        ...($process.env.options || {}),
-                        connectType: 'qr-code'
-                    });
-                } else {
-                    $process.send({
-                        content: {
-                            type: 'connection:close',
-                            data: {
-                                reasonCode: reason,
-                                reasonMessage: lastDisconnect
-                                    ?.error?.message ??
-                                    'Unknown reason'
-                            }
-                        }
-                    });
-                    if (reason !== DisconnectReason.loggedOut)
-                        await StartBot({
-                            ...($process.env.options || {}),
-                            connectType: 'qr-code'
-                        });
-                }
-            }
-
-            if (connection === 'open') $process.send({
-                content: {
-                    type: 'connection:open',
-                    data: {
-                        ...data,
-                        ...sock.user,
-                        isConnected: true,
-                        id: sock.user.id.split(":")[0]
-                            + "@s.whatsapp.net"
-                    }
-                }
-            })
-
-            if (qr) $process.send({
-                content: {
-                    event: 'pairing:qr-code',
-                    data: {
-                        rawQrCode: qr,
-                        qrCodeImage: await qrCode.toDataURL(qr),
-                        qrCodeText: await new Promise((resolve) =>
-                            qrTerminal.generate(qr, { small: true },
-                                (qrCode) => resolve(qrCode)))
-                    }
-                }
-            })
-        })
-
-        sock.ev.on('call', async (call) => {
-            const callInfo = call[0]
-            if (callInfo.status === 'offer') {
-                await sock.rejectCall(callInfo.id, callInfo.from)
-            }
-        })
-
-        sock.ev.on('messages.upsert', async (m) => {
-            try {
-                const chatUpdate = handlerLoader
-                    .get('core.handler.js')
-                await chatUpdate.default(m, sock)
-            } catch (e) {
-                logger.error(e);
-            }
-        })
+    fs.mkdirSync(folder, { recursive: true });
+    for (const _folder of ['creds', 'store', 'temp']) {
+        fs.mkdirSync(path.join(folder, _folder),
+            { recursive: true });
     }
 
-    await StartBot()
-} catch (e) {
-    logger.error(e);
-    new Error(e)
+    for (const key of Object.keys(process.env)) {
+        const env = process.env[key]
+        try { process.env[key] = JSON.parse(env) }
+        catch { process.env[key] = env }
+    }
+
+    process.on('uncaughtException', (err) => {
+        console.error(chalk.bgRedBright('[ERROR]'), err);
+    });
+
+    process.on('unhandledRejection', (reason, promise) => {
+        console.error(chalk.bgRedBright('[REJECTION]'),
+            promise, 'reason:', reason);
+    });
 }
+
+// library
+import { MakeClient } from '../library/client.js';
+import { Plugins } from '../library/plugins.js';
+import handlerLoader from '../library/loader.js';
+import { SubBots } from '../library/bots.js';
+
+// tmp
+await import('../library/purge.js')
+const _path = global.$dir_main
+
+await handlerLoader.loadFiles();
+
+const plugins = new Plugins(_path.plugins, {
+    subBots: false,
+    usePrefix: true,
+    stubtype: false,
+    command: false,
+});
+
+const subBots = new SubBots(
+    './storage/subBots', plugins);
+
+await plugins.load()
+
+// start
+async function StartBot() {
+    const mainBot = new MakeClient();
+
+    mainBot.events.on('connection', async (update) => {
+        process.send(update)
+    });
+
+    const sock = await mainBot.start({
+        folderPath: _path.creds,
+        ...process.env.connOptions
+    });
+
+    mainBot.events.on('messages', async (m) => {
+        if (!mainBot.sock.plugins) mainBot.sock.plugins = plugins;
+        if (!mainBot.sock.subBots) mainBot.sock.subBots = subBots;
+
+        mainBot.sock.subBot = false;
+
+        try {
+            const chatUpdate = handlerLoader
+                .get('core.handler.js')
+            await chatUpdate.default(m, sock)
+        } catch (e) {
+            console.error(e);
+        }
+    })
+
+    sock.ev.on('call', async (call) => {
+        const callInfo = call[0]
+        if (callInfo.status === 'offer') await sock
+            .rejectCall(callInfo.id, callInfo.from)
+    })
+}
+
+await StartBot()
